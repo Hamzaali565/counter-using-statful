5bdb5d87732892aea993f1660ed440479410ce16
Hamzaali565 <ha6153806@gmail.com>
2022-10-19 12:14:18 +0500

    more updates

--- src/component/index.jsx
+++ src/component/index.jsx
@@ -1,16 +1,22 @@
+import { logDOM } from "@testing-library/react";
 import React from "react";
 import { useState } from "react";
 import './index.css'
 const Counter = () => {
     let [isLit, setLit] = useState(1);
-
-    // https://michalsnik.github.io/aos/
+    let [bri, noBri] = useState(true);
+    const Change = ()=>{
+        noBri(!bri)
+    } 
+    // https://michalsnik.github.io/aos/ 
     return (
-        <div className="main">
-<h1 className="head">Counter App</h1>
-            <div className="numCen">{isLit}</div>
+        <div className={`${(bri) ? "main" : "main1"}`}>
+<h1 className={`${(bri) ? "head" : "head1"}`}>Counter App</h1>
+<button onClick ={Change} className="butt">change mode</button>
 
-            <div className="buttons">
+            <div className={`${(bri) ? "numCen" : "numCen1"}`}>{isLit}</div>
+
+            <div className={`${(bri) ? "buttons" : "buttons1"}`}>
             <button onClick={() => {
                 setLit(isLit + 1)
             }}>Add</button>
